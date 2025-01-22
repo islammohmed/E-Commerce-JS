08305bb0d2f8e1a3e8eaa6b217434dd1e525271d
Mariam Alaa <mariamalaa2019@gmail.com>
2025-01-22 22:01:01 +0200

    Mariam after merging with Donia

--- src/js/script.js
+++ src/js/script.js
@@ -1,79 +1,143 @@
 // ^ =========Navbar menu mobile =================
-const mobileMenuButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
 });
 // ^========Display All Products =================
-(function(){
-    var myhttp = new XMLHttpRequest();
-    myhttp.addEventListener("readystatechange", function(){
-        console.log(myhttp.readyState);
-        if(myhttp.readyState == 4 && myhttp.status == 200){
-            displayAllProduct(JSON.parse(myhttp.response));
-        }
-    });
-    myhttp.open("GET", "https://fakestoreapi.com/products", true);
-    myhttp.send();
+(function () {
+  var myhttp = new XMLHttpRequest();
+  myhttp.addEventListener("readystatechange", function () {
+    console.log(myhttp.readyState);
+    if (myhttp.readyState == 4 && myhttp.status == 200) {
+      displayAllProduct(JSON.parse(myhttp.response));
+    }
+  });
+  myhttp.open("GET", "https://fakestoreapi.com/products", true);
+  myhttp.send();
 
-    function displayAllProduct(list){
-        let products = ' ';
-        for(let i = 0; i < list.length; i++){
-            products += `<div class="w-[30%] sm:w-1/2 md:w-[30%] lg:w-[30%] min-w-[250px] p-3 bg-white border border-[#cce7d0] rounded-[25px] cursor-pointer shadow-[20px_20px_30px_rgba(0,0,0,0.02)] my-4 transition-transform duration-300 ease-in-out relative hover:shadow-[20px_20px_54px_rgba(28,77,161,0.06)] hover:scale-105">
+  function displayAllProduct(list) {
+    let products = " ";
+    for (let i = 0; i < list.length; i++) {
+      products += `<div class="w-[30%] sm:w-1/2 md:w-[30%] lg:w-[30%] min-w-[250px] p-3 bg-white border border-[#cce7d0] rounded-[25px] cursor-pointer shadow-[20px_20px_30px_rgba(0,0,0,0.02)] my-4 transition-transform duration-300 ease-in-out relative hover:shadow-[20px_20px_54px_rgba(28,77,161,0.06)] hover:scale-105">
                 <a href="#">
-                    <img class="w-full rounded-[20px] h-80  object-contain" src="${list[i].image}" alt="${list[i].title}">
+                    <img class="w-full rounded-[20px] h-80  object-contain" src="${
+                      list[i].image
+                    }" alt="${list[i].title}">
                     <div class="text-left py-2">
-                        <span class="text-[#606063] text-xs">${list[i].category}</span>
-                        <h5 class="pt-2 text-[#1a1a1a] text-sm">${list[i].title}</h5>
+                        <span class="text-[#606063] text-xs">${
+                          list[i].category
+                        }</span>
+                        <h5 class="pt-2 text-[#1a1a1a] text-sm">${
+                          list[i].title
+                        }</h5>
                         <div class="flex">
-                            ${'<i class="bx bxs-star text-[#f3b519] text-xs"></i>'.repeat(5)}
+                            ${'<i class="bx bxs-star text-[#f3b519] text-xs"></i>'.repeat(
+                              5
+                            )}
                         </div>
-                        <h4 class="pt-2 text-xs font-bold text-[#088178]">${list[i].price} $</h4>
+                        <h4 class="pt-2 text-xs font-bold text-[#088178]">${
+                          list[i].price
+                        } $</h4>
                     </div>
                     <a href="#" class="absolute bottom-5 right-2 w-10 h-10 flex items-center justify-center rounded-full bg-[#e8f6ea] font-medium text-[#088178] border border-[#cce7d0]">
                         <i class='bx bx-cart'></i>
                     </a>
                 </a>
             </div>`;
-        }
-        document.querySelector("#product").innerHTML = products;
     }
+    document.querySelector("#product").innerHTML = products;
+  }
 })();
 
-
 // ^========Display New Arrival Product  =================
-(function(){
-    var myhttp = new XMLHttpRequest();
-    myhttp.addEventListener("readystatechange", function(){
-        console.log(myhttp.readyState);
-        if(myhttp.readyState == 4 && myhttp.status == 200){
-            displayProduct(JSON.parse(myhttp.response));
-        }
-    });
-    myhttp.open("GET", "https://fakestoreapi.com/products?sort=desc&limit=4", true);
-    myhttp.send();
+(function () {
+  var myhttp = new XMLHttpRequest();
+  myhttp.addEventListener("readystatechange", function () {
+    console.log(myhttp.readyState);
+    if (myhttp.readyState == 4 && myhttp.status == 200) {
+      displayProduct(JSON.parse(myhttp.response));
+    }
+  });
+  myhttp.open(
+    "GET",
+    "https://fakestoreapi.com/products?sort=desc&limit=4",
+    true
+  );
+  myhttp.send();
 
-    function displayProduct(list){
-        let products = ' ';
-        for(let i = 0; i < list.length; i++){
-            products += `<div class="w-[20%] sm:w-1/2 md:w-[20%] lg:w-[20%] min-w-[200px] p-3 bg-white border border-[#cce7d0] rounded-[25px] cursor-pointer shadow-[20px_20px_30px_rgba(0,0,0,0.02)] my-4 transition-transform duration-300 ease-in-out relative hover:shadow-[20px_20px_54px_rgba(28,77,161,0.06)] hover:scale-105">
+  function displayProduct(list) {
+    let products = " ";
+    for (let i = 0; i < list.length; i++) {
+      products += `<div class="w-[20%] sm:w-1/2 md:w-[20%] lg:w-[20%] min-w-[200px] p-3 bg-white border border-[#cce7d0] rounded-[25px] cursor-pointer shadow-[20px_20px_30px_rgba(0,0,0,0.02)] my-4 transition-transform duration-300 ease-in-out relative hover:shadow-[20px_20px_54px_rgba(28,77,161,0.06)] hover:scale-105">
                         <a href="#">
-                            <img class="w-full rounded-[20px] h-80  object-contain" src="${list[i].image}" alt="${list[i].title}">
+                            <img class="w-full rounded-[20px] h-80  object-contain" src="${
+                              list[i].image
+                            }" alt="${list[i].title}">
                             <div class="text-left py-2">
-                                <span class="text-[#606063] text-xs">${list[i].category}</span>
-                                <h5 class="pt-2 text-[#1a1a1a] text-sm">${list[i].title}</h5>
+                                <span class="text-[#606063] text-xs">${
+                                  list[i].category
+                                }</span>
+                                <h5 class="pt-2 text-[#1a1a1a] text-sm">${
+                                  list[i].title
+                                }</h5>
                                 <div class="flex">
-                                    ${'<i class="bx bxs-star text-[#f3b519] text-xs"></i>'.repeat(5)}
+                                    ${'<i class="bx bxs-star text-[#f3b519] text-xs"></i>'.repeat(
+                                      5
+                                    )}
                                 </div>
-                                <h4 class="pt-2 text-xs font-bold text-[#088178]">${list[i].price} $</h4>
+                                <h4 class="pt-2 text-xs font-bold text-[#088178]">${
+                                  list[i].price
+                                } $</h4>
                             </div>
                             <a href="#" class="absolute bottom-5 right-2 w-10 h-10 flex items-center justify-center rounded-full bg-[#e8f6ea] font-medium text-[#088178] border border-[#cce7d0]">
                                 <i class='bx bx-cart'></i>
                             </a>
                         </a>
                     </div>`;
-        }
-        document.querySelector("#newProduct").innerHTML = products;
     }
+    document.querySelector("#newProduct").innerHTML = products;
+  }
 })();
 
+var myhttp = new XMLHttpRequest();
+myhttp.open("GET", "https://fakestoreapi.com/products/categories", true);
+myhttp.send();
+myhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] === "electronics") {
+        document.getElementById(
+          "card"
+        ).innerHTML += `<a href="https://google.com" target="_blank">
+      <div class="border-2 border-gray-300 rounded-lg p-7 m-4">
+            <img class="w-full rounded-[20px] h-80  object-contain" src="assets/e.png"/> 
+            <h2 class="text-center text-3xl font-medium mt-4">${data[i]}</h2> </div></a>`;
+      } else if (data[i] === "jewelery") {
+        document.getElementById(
+          "card"
+        ).innerHTML += `<a href="https://google.com" target="_blank">
+       <div class="border-2 border-gray-300 rounded-lg p-7 m-4"> 
+        <img class="w-full rounded-[20px] h-80  object-contain" src="assets/jew.jfif"/>
+            <h2 class="text-center text-3xl font-medium mt-4">${data[i]}</h2> </div></a>`;
+      } else if (data[i] === "men's clothing") {
+        document.getElementById(
+          "card"
+        ).innerHTML += `<a href="https://google.com" target="_blank">
+        <div class="border-2 border-gray-300 rounded-lg p-7 m-4">
+    <img class="w-full rounded-[20px] h-80  object-contain" src="assets/men.jfif"/> 
+    <h2 class="text-center text-3xl font-medium mt-4">${data[i]}</h2> </div>
+    </a>`;
+      } else if (data[i] == "women's clothing") {
+        document.getElementById(
+          "card"
+        ).innerHTML += ` <a href="https://google.com" target="_blank">
+        <div class="border-2 border-gray-300 rounded-lg p-7 m-4">
+    <img class="w-full rounded-[20px] h-80  object-contain" src="assets/women.jfif"/>
+    <h2 class="text-center text-3xl font-medium mt-4">${data[i]}</h2></div> </a>`;
+      }
+    }
+  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-
     extend: {
       fontFamily: {
         main: ["Raleway", "sans-serif"],
       },
       backgroundImage: {
-        hero: "url('/src/assets/hero.jpg')",
-        button: "url('/src/assets/button.png')",
-        banner: "url('/src/assets/banner.png')",
+        hero: "url('/src/assets/hero.jfif')",
+        button: "url('/src/assets/button.jfif')",
+        banner: "url('/src/assets/banner.jfif')",
       },
+    },
   },
   plugins: [],
 };
